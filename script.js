5dd80271cb16af14c88229af277c3ce0fcd41211
gitAash <153607773+gitAash@users.noreply.github.com>
2024-01-15 15:00:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function showDetails(name, image, price) {
+    alert(name + '\nPrice: $' + price);
+  }
+  
+  function navigateToOtherPage() {
+    // Replace 'otherPage.html' with the actual page you want to navigate to
+    window.location.href = 'otherPage.html';
+  }
+  
