6cc92837ea4bcc0cb6d2880929f90997feb64764
Abhignanew <120008380+Abhignanew@users.noreply.github.com>
2024-01-21 22:25:49 +0530

    Add files via upload
    Added script to handle if the entered password and confirm password is matching, added backend through php, and the data is being stored in the dataset, also after successful registration the webpage will move to menu page.

---
+++ process_registration.php
@@ -0,0 +1,33 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Retrieve form data
+    $firname = $_POST["firname"];
+    $lasname= $_POST["lasname"];
+    $email = $_POST["email"];
+    $password = $_POST["password"];
+    $address = $_POST["address"];
+    $phoneNumber = $_POST["phoneNumber"];
+
+    // Perform validation (you can add more validation as needed)
+    if (empty($firname) || empty($lasname) || empty($email) || empty($password) || empty($address) || empty($phoneNumber)) {
+        echo "All fields are required.";
+    } else {
+        // Database connection
+$conn=new mysqli('localhost','root','Not_working2','restaurantdb');
+if ($conn->connect_error) {
+    die('Connection Failed:'. $conn->connect_error);
+}
+else{
+    $stmt = $conn->prepare("INSERT INTO resto_users (firname, lasname, email, password, address, phoneNumber) VALUES (?, ?, ?, ?, ?, ?)");
+    $stmt->bind_param("sssssi",$firname,$lasname,$email,$password,$address,$phoneNumber);
+
+$stmt->execute();
+header("Location: menu1.html"); 
+        
+$stmt->close();
+$conn->close();
+}
+        
+    }
+}
+?>
