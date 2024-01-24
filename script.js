946d2255ad3f735904df46f204ee1e8c97d872b8
Akshayansg24 <156597963+Akshayansg24@users.noreply.github.com>
2024-01-24 22:43:48 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -9,12 +9,45 @@ menubar.onclick = () =>{
     
 }
 
+document.addEventListener("DOMContentLoaded", function() {
+  const ratingInputs = document.querySelectorAll('input[name="rating"]');
+  const feedbackMessage = document.getElementById('feedbackMessage');
+  
+  ratingInputs.forEach(input => {
+      input.addEventListener('change', function() {
+          const selectedRating = this.value;
+          feedbackMessage.textContent = `You rated ${selectedRating} star(s),Thank you!!.`;
+      });
+  });
+});
+document.addEventListener("DOMContentLoaded", function() {
+  const allBranches = document.querySelectorAll('.branch');
+
+  allBranches.forEach(branch => {
+    branch.addEventListener('click', function() {
+      const randomMovement = (Math.random() - 0.5) * 100; 
+      this.style.transform = `translate(${randomMovement}px, -5px)`;
+      setTimeout(() => {
+        this.style.transform = 'translate(0, -5px)';
+      }, 1000); // Reset the movement after 1 second
+    });
+  });
+});
+document.getElementById('contactForm').addEventListener('submit', function (event) {
+  event.preventDefault();
+  // Add your form submission logic here
+  alert('Form submitted!');
+});
+
+
+
+
 
 const sr = ScrollReveal ({
     distance : '45px',
     duration : 2700,
     reset : true,
-  })
+})
 
   sr.reveal('.myimageabout',{ delay:350, origin:'left' })
   sr.reveal('.about-text-content',{ delay:350, origin:'right' })
@@ -29,7 +62,8 @@ const sr = ScrollReveal ({
   sr.reveal('.food-main-content',{ delay:350, origin:'bottom' })
   sr.reveal('.before',{ delay:350, origin:'bottom' })
   sr.reveal('.footer-logo',{ delay:350, origin:'bottom' })
-
+  
+  
 
 
 
